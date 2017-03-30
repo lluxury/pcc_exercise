a71167ea553d93af80802257342cfc3426dec8fa
yann <tmp4321@qq.com>
2017-03-30 17:56:14 +0800

    3-5 modify guest list

--- 03/invited_guests.py
+++ 03/invited_guests.py
@@ -2,3 +2,8 @@ guests_list = ['laobian', 'wenhao1','wenhao2']
 for name in guests_list:
     print ("I would like to invite " + name + " to dinner")
 
+print (guests_list[1] + " could't make it for the appointment.")
+
+guests_list[1] = ('tianjia')
+for name in guests_list:
+    print ("I would like to invite " + name + " to dinner")
