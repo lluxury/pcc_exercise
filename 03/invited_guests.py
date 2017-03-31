2665435bf8d2668f5d317b57094e62605ee5af47
yann <tmp4321@qq.com>
2017-03-31 10:52:57 +0800

    3-7 remove guest

--- 03/invited_guests.py
+++ 03/invited_guests.py
@@ -13,3 +13,12 @@ guests_list.insert(2, 'middle')
 guests_list.append('last')
 for name in guests_list:
     print("I would like to invite " + name + " to dinner")
+print("\nI could only invite two guest for dinner.")
+for name in guests_list[:-2]:
+    sorry_guest = guests_list.pop()
+    print("sorry, " + sorry_guest + " I can't invite you for dinner")
+for name in guests_list:
+    print("I would like to invite " + name + " to dinner")
+del guests_list[0]
+del guests_list[0]
+print(guests_list)
