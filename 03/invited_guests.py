117e6e3a4de1cc31a5b9df8e7c4aba300d7d0812
yann <tmp4321@qq.com>
2017-03-31 10:24:56 +0800

    3-6 add guest

--- 03/invited_guests.py
+++ 03/invited_guests.py
@@ -2,8 +2,14 @@ guests_list = ['laobian', 'wenhao1','wenhao2']
 for name in guests_list:
     print ("I would like to invite " + name + " to dinner")
 
-print (guests_list[1] + " could't make it for the appointment.")
+print (guests_list[1] + " could't make it for the appointment.\n")
 
 guests_list[1] = ('tianjia')
 for name in guests_list:
     print ("I would like to invite " + name + " to dinner")
+print("\nI found a bigger dining table")
+guests_list.insert(0, 'first')
+guests_list.insert(2, 'middle')
+guests_list.append('last')
+for name in guests_list:
+    print("I would like to invite " + name + " to dinner")
