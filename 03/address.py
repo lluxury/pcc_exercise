30014487766cbb1beb97513b0357726692f08033
yann <tmp4321@qq.com>
2017-03-31 15:09:28 +0800

    3-8 address

---
+++ 03/address.py
@@ -0,0 +1,18 @@
+address = ['japan', 'australia','usa', 'belgium', 'bolivia']
+print(address)
+print(sorted(address))
+print(address)
+print(sorted(address, reverse=True))
+print(address)
+
+address.reverse()
+print(address)
+
+address.reverse()
+print(address)
+
+address.sort()
+print(address)
+
+address.sort(reverse=True)
+print(address)
