a0f6b37f6a9397aa56b532dd0f50c25f3dc597ae
yann <tmp4321@qq.com>
2017-04-01 15:27:09 +0800

    4-13 buffet

---
+++ 04/breakfast.py
@@ -0,0 +1,8 @@
+my_foods = ('Buns','fried dumplings', 'twist', 'soy milk', 'biscuits')
+for my_food in my_foods:
+    print(my_food)
+#my_foods[0] = 'xx'
+print('\n')
+my_foods = ('Buns1','fried dumplings2', 'twist', 'soy milk', 'biscuits')
+for my_food in my_foods:
+    print(my_food)
