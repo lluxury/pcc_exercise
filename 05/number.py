9cb059d942319b94c91c8028523ee00d826b5551
yann <tmp4321@qq.com>
2017-04-06 17:16:35 +0800

    5-11 number 1-10

---
+++ 05/number.py
@@ -0,0 +1,15 @@
+numbers = [i for i in [1-9]
+new_users = ['dqx', 'bql', 'xyz1', 'txo', 'admin']
+    
+if new_users:
+        #print('We need to find some users!')
+    for new_user in new_users:
+
+        if new_user.lower() in  numbers:
+  i for i in [1-9st')
+        else:
+            print('Hello ' + new_user +', is avilable')
+else:
+    print('We need to find some users!')
+
+#注意列表的表达方式
