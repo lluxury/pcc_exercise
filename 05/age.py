8a99ca9a75e589bdb53da5eb2d9978c4abe2bdaf
yann <tmp4321@qq.com>
2017-04-06 15:55:29 +0800

    5-6 human

---
+++ 05/age.py
@@ -0,0 +1,17 @@
+age = 68
+if age < 2:
+    print("he is a baby")
+elif age >=2 and age <4:
+    print("he walk")
+elif age >=4 and age <13:
+    print("he is a children")
+
+elif age >=13 and age <20:
+    print("he is a teen-ageer")
+
+
+elif age >=20 and age <65:
+    print("he is a human")
+else:
+    print("he is older")
+
