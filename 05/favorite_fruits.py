45a18cf995426bdb84af180fd93f831bf5af126d
yann <tmp4321@qq.com>
2017-04-06 16:02:37 +0800

    5-7 faorit fruits

---
+++ 05/favorite_fruits.py
@@ -0,0 +1,15 @@
+favorite_fruits = ['apple', 'banana', 'orange']
+if 'apple' in favorite_fruits:
+    print('You really like bananas!')
+
+if 'orange' in favorite_fruits:
+    print('You really like bananas!')
+
+if 'banana' in favorite_fruits:
+    print('You really like bananas!')
+
+if 'apple' in favorite_fruits:
+    print('You really like bananas!')
+
+if 'apple' in favorite_fruits:
+    print('You really like bananas!')
