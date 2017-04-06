048bba22d270ea9af099867562b53e867d3745d3
yann <tmp4321@qq.com>
2017-04-06 16:49:50 +0800

    5-9 say_hello_to_nobody

--- 05/say_hello.py
+++ 05/say_hello.py
@@ -1,6 +1,14 @@
 name_list = ['dqx', 'bql', 'xyz', 'txo', 'admin']
-for name in name_list:
-    if name == 'admin':
-        print('Hello admin, would you like to see a status report?')
-    else:
-        print('Hello ' + name +', thank you for logging in again')
+    
+if name_list:
+        #print('We need to find some users!')
+    for name in name_list:
+
+        if name == 'admin':
+            print('Hello admin, would you like to see a status report?')
+        else:
+            print('Hello ' + name +', thank you for logging in again')
+else:
+    print('We need to find some users!')
+
+#注意列表的表达方式
