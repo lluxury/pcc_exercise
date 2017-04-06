5465b8ec6835461802554b11cf2e2cb4070a0fc4
yann <tmp4321@qq.com>
2017-04-06 15:27:36 +0800

    5-5 alien_color3

--- 05/alien_color.py
+++ 05/alien_color.py
@@ -1,14 +1,27 @@
 alien_color = 'green'
 if alien_color == 'green':
     print("player get 5 point")
-
-else:
-    print("player get 10 point")
-
-
-alien_color = 'green'
-if alien_color == 'green1':
+elif alien_color == 'yellow':
     print("player get 5 point")
 
 else:
+    print("player get 15 point")
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("player get 5 point")
+elif alien_color == 'yellow':
     print("player get 10 point")
+
+else:
+    print("player get 15 point")
+
+
+alien_color = 'red'
+if alien_color == 'green':
+    print("player get 5 point")
+elif alien_color == 'yellow':
+    print("player get 5 point")
+
+else:
+    print("player get 15 point")
