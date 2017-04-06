07c78b33a606621f3623d72142bbace5e47a120b
yann <tmp4321@qq.com>
2017-04-06 15:24:56 +0800

    5-4 alien_color

--- 05/alien_color.py
+++ 05/alien_color.py
@@ -2,6 +2,13 @@ alien_color = 'green'
 if alien_color == 'green':
     print("player get 5 point")
 
-alien_color = 'yellow'
-if alien_color == 'green':
-    print("\nplayer get 5 point")
+else:
+    print("player get 10 point")
+
+
+alien_color = 'green'
+if alien_color == 'green1':
+    print("player get 5 point")
+
+else:
+    print("player get 10 point")
