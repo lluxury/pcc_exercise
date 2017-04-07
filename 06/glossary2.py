3a527d167d72e781aa29459610340ed58c9bf5c4
yann <tmp4321@qq.com>
2017-04-07 15:40:30 +0800

    6-4 glossary-2

---
+++ 06/glossary2.py
@@ -0,0 +1,16 @@
+friend = {
+'printf': 'print', 
+'elif': 'if',
+'END': 'end', 
+'BEGIN':'begin', 
+'break':'stop',
+'break1':'stop1',
+'BEGIN1':'begin2', 
+'BEGIN3':'begin2', 
+'BEGIN4':'begin2', 
+'BEGIN5':'begin2', 
+}
+
+for key, value in friend.items():
+    print("\nkey \t" + key)
+    print("value \t" + value)
