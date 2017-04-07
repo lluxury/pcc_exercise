eacbf0d616b1d77bd5df5ab5b77d6a3ba8d91804
yann <tmp4321@qq.com>
2017-04-07 11:47:11 +0800

    glossary

---
+++ 06/glossary.py
@@ -0,0 +1,7 @@
+friend = {'printf': 'print', 'elif': 'if','END': 'end', 'BEGIN':'begin', 'break':'stop'}
+
+print('printf:\t'+friend['printf'])
+print('elif:\t'+friend['elif'])
+print('END:\t'+friend['END'])
+print('BEGIN:\t'+friend['BEGIN'])
+print('break:\t'+friend['break'])
