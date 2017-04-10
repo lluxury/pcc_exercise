2cb654c9a7ff0b0add3e3db6c7ac3ab75c3344a1
yann <tmp4321@qq.com>
2017-04-10 17:39:07 +0800

    6-7 friend2

---
+++ 06/friend2.py
@@ -0,0 +1,11 @@
+friend = {'first_name': 'li', 'last_name': 'cl','age': '28', 'city':'sh'}
+friend2 = {'first_name': 'li2', 'last_name': 'cl2','age': '28', 'city':'sh'}
+friend3 = {'first_name': 'li3', 'last_name': 'cl3','age': '28', 'city':'sh'}
+
+friend_all = [friend,friend2,friend3]
+# print(friend['first_name'])
+# print(friend['last_name'])
+# print(friend['age'])
+# print(friend['city'])
+for friend_echo in friend_all:
+    print(friend_echo)
