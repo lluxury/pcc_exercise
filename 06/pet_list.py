f72c3ce3d8d50665aa0b6d28d0d860401b20ba8d
yann <tmp4321@qq.com>
2017-04-10 17:44:22 +0800

    6-8 pet_list

---
+++ 06/pet_list.py
@@ -0,0 +1,12 @@
+pet1 = {'type': 'cat', 'owner': 'cl'}
+pet2 = {'type': 'dog', 'owner': 'cl2'}
+pet3 = {'type': 'mouse', 'owner': 'cl3'}
+
+
+pets = [pet1,pet2,pet3]
+# print(friend['first_name'])
+# print(friend['last_name'])
+# print(friend['age'])
+# print(friend['city'])
+for pet in pets:
+    print(pet)
