17381cdde941961fe7a993774d99c28541b4249d
yann <tmp4321@qq.com>
2017-04-11 17:28:25 +0800

    7-4 pizza_ingregents

---
+++ 07/pizza_ingredents.py
@@ -0,0 +1,6 @@
+prompt = "\nWhat kind of ingredients do you want "
+prompt += "\nEnter 'quit' to end of program."
+messeage = ""
+while messeage !='quit':
+    messeage = input(prompt)
+    print("We will add "+ messeage + " with you pizza")
