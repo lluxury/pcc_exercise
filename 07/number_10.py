ee7f678144c10c9b629ad3e4ad0bb387163dc586
yann <tmp4321@qq.com>
2017-04-11 17:17:58 +0800

    7-3 number 10

---
+++ 07/number_10.py
@@ -0,0 +1,5 @@
+number=input("Please input a number:\n")
+if int(number) %10==0:
+    print("This number is an integer multiple of 10")
+else:
+    print("This number is not an integer multiple of 10")
