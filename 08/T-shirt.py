1fc9d0d39da6b114ffddd161d46647d97a0dbefa
yann <tmp4321@qq.com>
2017-04-13 16:23:43 +0800

    8-3 t-shirt

---
+++ 08/T-shirt.py
@@ -0,0 +1,10 @@
+def make_shirt(size, word):
+    '''What do I Lent in this chapter?'''
+    print('size is  ' + size +', word is ' + word)
+
+make_shirt('8',"Alice in Wonderland")
+
+
+#不能直接用int型数字,要用字符
+
+make_shirt(word = "Alice in Wonderland", size = '8')
