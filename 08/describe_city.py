fa3f7c18e5a24f2521ff2c04ffe60b4f7fd3d638
yann <tmp4321@qq.com>
2017-04-13 17:50:51 +0800

    8-5 08/describe_city

---
+++ 08/describe_city.py
@@ -0,0 +1,11 @@
+def describe_city(name, country='China'):
+    '''What do I Lent in this chapter?'''
+    print('name is  ' + name +', country is ' + country)
+
+describe_city('sh')
+
+
+#不能直接用int型数字,要用字符
+
+describe_city('bj')
+describe_city('tokyo','jp')
