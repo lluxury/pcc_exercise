ee5e2818fbec0f6d27c6ff1365c64313deac1640
yann <tmp4321@qq.com>
2017-04-13 15:54:10 +0800

    8-2 favorite book

---
+++ 08/favorite_book.py
@@ -0,0 +1,7 @@
+def favorite_book(title):
+    '''What do I Lent in this chapter?'''
+    print('One of my favorite books is ' + title)
+
+favorite_book("Alice in Wonderland")
+
+#title 形参, Alice 实参
