fabec9c6b7afe24826c37ffddf805d8a58dcdd44
yann <tmp4321@qq.com>
2017-04-14 16:03:44 +0800

    8-9 user_album

--- 08/user_album.py
+++ 08/user_album.py
@@ -12,23 +12,27 @@ def make_album(name, album='L.P', number=''):
         repet = input('\nwould like to quit?')
         if repet == 'quit':
             polling_active = False
-        print(album_list)
+        #print(''summay'') x
+    for name ,album  in album_list.items():
+        print(name + '\t' + album + '\n' )
     return album_list
 
 
 
-care=make_album('bqb', 'bb')
-print(care)
+#care=make_album('bqb', 'bb')
+#print(care)
 
-xiaoxin = make_album('bj')
+xiaoxin = make_album('')
 #print(xiaoxin)
+#反回值是字典,下面可以继续操作
 
-zhu=make_album('zdmx','xzc','8')
-print(zhu)
+#zhu=make_album('zdmx','xzc','8')
+#print(zhu)
 
 #print (album_list)
 #函数里的字典,调用不能用,也就是不能用返回值以外的
 #字典是单次的,对于每个参数是独立的
 #返回值的用法忘掉了,重新回忆下
 #不清字典的话,重复运行会累计,格式也有问题
+#字典的输出都很乱,人家遍历的
 #n次排错,格式,对齐,开关的写法,还是要多写啊
