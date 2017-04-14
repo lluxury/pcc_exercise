559882ecb22e404bcc2c5a28cf0c2150bf9a07ee
yann <tmp4321@qq.com>
2017-04-14 16:45:24 +0800

    8-10 the great magician

--- 08/show_magicians.py
+++ 08/show_magicians.py
@@ -4,8 +4,19 @@ def show_magicians(names):
         message = "the great :" + name
         print(message)
 
+def make_great(old,new):
+    '''plus the great for each element in old list'''
+    while old:
+        change_one = old.pop()
+        change_one = 'the great' + change_one
+        new.append(change_one)
+        pass
+
+    pass
 
 name_list = ['dwfeb', 'lj', 'bdp']
-show_magicians(name_list)
+new_list = []
+make_great(name_list, new_list)
+show_magicians(new_list)
 
 #忘记加参数,导致报错
