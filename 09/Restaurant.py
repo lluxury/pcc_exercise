3bbd19e86cb8f934e3974b47d27aa7c561bedaaa
yann <tmp4321@qq.com>
2017-04-17 14:44:58 +0800

    9-2 3restaurant

--- 09/Restaurant.py
+++ 09/Restaurant.py
@@ -21,3 +21,9 @@ print ("my Restaurant's restaurant_name is " + my_Restaurant.restaurant_name.tit
 print ("my Restaurant's cuisine_type is " + my_Restaurant.cuisine_type.title())
 my_Restaurant.describe_user()
 my_Restaurant.open_restaurant()
+
+my_Restaurant2 = Restaurant('kfc', 'fastfood')
+my_Restaurant2.describe_user()
+
+my_Restaurant3 = Restaurant('Mc_donalds', 'fastfood')
+my_Restaurant3.describe_user()
