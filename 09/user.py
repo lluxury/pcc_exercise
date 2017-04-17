323fe17708139931c98a05c6801356f8c4d6e5bd
yann <tmp4321@qq.com>
2017-04-17 16:15:07 +0800

    9-5 attemp login

--- 09/user.py
+++ 09/user.py
@@ -5,17 +5,30 @@ class user():
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+        self.login_attempts=0
 
     def describe_user(self):
-        '''模拟小狗被命令时蹲下'''
+        ''' '''
         print(" The name of user is " + self.first_name.title() + ' ' + self.last_name.title())
         print(" user age is " + self.age.title())
 
     def greet_user(self):
-        '''模拟小狗被命令打滚'''
+        ''''''
         print('Hello ' + self.first_name.title() + ' ' + self.last_name.title() + "!")
         pass
+    def read_login_attemps(self):
+        '''read '''
+        print(self.login_attempts)
         
+    def increment_login_attempts(self):
+        ''' '''
+        self.login_attempts+=1
+        pass
+
+    def reset_login_attempts(self):
+        '''类的方法是不能缺少self的'''
+        self.login_attempts=0
+        pass
 
 my_user=user('weilian', 'x','6','y')
 print ("my user's first_name is " + my_user.first_name.title())
@@ -26,3 +39,9 @@ my_user.greet_user()
 my_user1=user('wangzi', 'x','6','y')
 my_user1.describe_user()
 my_user1.greet_user()
+
+my_user1.increment_login_attempts()
+my_user1.increment_login_attempts()
+my_user1.read_login_attemps()
+my_user1.reset_login_attempts()
+my_user1.read_login_attemps()
