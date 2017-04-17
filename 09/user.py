ffb1749c59843f7f710aae7e8cfd03606a06fcae
yann <tmp4321@qq.com>
2017-04-17 15:02:50 +0800

    9-3 user

---
+++ 09/user.py
@@ -0,0 +1,24 @@
+class user():
+    """docstring for user"""
+    def __init__(self, first_name, last_name, age, gender):
+        #super(user, self).__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def describe_user(self):
+        '''模拟小狗被命令时蹲下'''
+        print(" The name of user is " + self.first_name.title() + ' ' + self.last_name.title())
+        print(" user age is " + self.age.title())
+
+    def greet_user(self):
+        '''模拟小狗被命令打滚'''
+        print('Hello ' + self.first_name.title() + ' ' + self.last_name.title() + "!")
+        pass
+        
+
+my_user=user('weilian', 'x','6','y')
+print ("my user's first_name is " + my_user.first_name.title())
+print ("my user's last_name is " + my_user.last_name.title())
+my_user.describe_user()
+my_user.greet_user()
