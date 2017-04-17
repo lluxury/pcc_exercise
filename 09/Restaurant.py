89227bf2c6159bdf850c9be33ed366b72217b118
yann <tmp4321@qq.com>
2017-04-17 15:28:19 +0800

    9-4 number of user

--- 09/Restaurant.py
+++ 09/Restaurant.py
@@ -1,20 +1,35 @@
 class Restaurant():
-    """docstring for Restaurant"""
+    """固定的值在函数入口的参数里没有"""
     def __init__(self, restaurant_name, cuisine_type):
         #super(Restaurant, self).__init__()
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_user(self):
-        '''模拟小狗被命令时蹲下'''
+        '''描述'''
         print(" The name of restaurant is " + self.restaurant_name.title())
         print(" The type of restaurant is " + self.cuisine_type.title())
 
     def open_restaurant(self):
-        '''模拟小狗被命令打滚'''
+        '''欢迎'''
         print(self.restaurant_name.title() + " is opening")
         pass
-        
+    
+    def set_number_served(self, number):
+        self.number_served = number
+        print(self.number_served)
+
+    def increment_number_served(self,plus):
+        #self.number_served += self.increment_number_served(plus)
+        self.number_served += plus
+        #print(self.number_served)
+
+    def read_number(self):
+        print(self.number_served)
+        pass
+
+
 
 my_Restaurant=Restaurant('weilian', '6')
 print ("my Restaurant's restaurant_name is " + my_Restaurant.restaurant_name.title())
@@ -27,3 +42,9 @@ my_Restaurant2.describe_user()
 
 my_Restaurant3 = Restaurant('Mc_donalds', 'fastfood')
 my_Restaurant3.describe_user()
+
+my_Restaurant.set_number_served(9)
+my_Restaurant.increment_number_served(18)
+my_Restaurant.read_number()
+
+#函数的操作要通过函数调用,读,改
