e057adf13910b439ed4a1307af5fc71a2f7cd71a
yann <tmp4321@qq.com>
2017-04-26 20:00:44 +0800

    decorator 2

--- 08/decorator.py
+++ 08/decorator.py
@@ -1,8 +1,20 @@
+def deco(func):
+    print("before myfunc() called.")
+    func()
+    print("  after myfunc() called.")
+    return func
+ 
 def myfunc():
-    print("myfunc() called.")
+    print(" myfunc() called.")
+ 
+myfunc = deco(myfunc)
  
 myfunc()
 myfunc()
 
-# myfunc() called.
-# myfunc() called.
+
+# before myfunc() called.
+#  myfunc() called.
+#   after myfunc() called.
+#  myfunc() called.
+#  myfunc() called
