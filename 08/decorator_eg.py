0ff105277e081300586d902718de4355cc97e042
yann <tmp4321@qq.com>
2017-04-28 16:49:17 +0800

    decorator 2

--- 08/decorator_eg.py
+++ 08/decorator_eg.py
@@ -1,18 +1,20 @@
 def salesgirl(method):
     def serve(*args):
         print "Salesgirl: Hello, what do you want?", method.__name__
-        method(*args)
+        return method(*args)
     return serve
 
 @salesgirl
 def try_this_shirt(size):
     if size <35:
         print "I: %d inches is to small to me" %(size)
+        return False
     else:
         print "I: %d inches is just enough" %(size)
+        return True
 
-try_this_shirt(38)
+result = try_this_shirt(38)
+print "Mum:dou you want to buy this ", result
 
-
-#三个函数, serve(*args)的父是salesgirl
-#执行装饰器,执行调用  ,method.__name__
+#希望获得结果,第4行是返回给最后1行的结果
+#先执行装饰器,然后执行调用,然后调用参数传到装饰器内部,然后执行函数
