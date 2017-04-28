d86c5feb2cd8f1fab911804f8c51c3a01c315a0c
yann <tmp4321@qq.com>
2017-04-28 16:50:07 +0800

    decorator 2

--- 08/decorator_eg.py
+++ 08/decorator_eg.py
@@ -18,3 +18,4 @@ print "Mum:dou you want to buy this ", result
 
 #希望获得结果,第4行是返回给最后1行的结果
 #先执行装饰器,然后执行调用,然后调用参数传到装饰器内部,然后执行函数
+#try_this_shirt方法需要改成带返回值 (假设是bool类型,True就是要买，False就是不想买)，那么salesgirl中的serve也应该带返回值，并且返回值就是 method(*args)
