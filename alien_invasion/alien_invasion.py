690f2aa7673c13b97d6d4402e05521b6eb8853c3
yann <tmp4321@qq.com>
2021-03-18 15:30:58 +0800

    blit ship

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,9 @@
+import sys
+
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 def run_game():
@@ -10,11 +13,17 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    ship = Ship(screen)
 
-    
     # Start the main loop for the game.
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
+
         pygame.display.flip()
 
 
