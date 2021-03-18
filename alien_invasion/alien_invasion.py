ce67d4cf037567c3c2003c3f29faa6c288e9af1d
yann <tmp4321@qq.com>
2021-03-18 15:09:53 +0800

    create class

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,21 +1,21 @@
-import sys
 import pygame
+
+from settings import Settings
+
 
 def run_game():
     # Initialize pygame, settings, and screen object.
     pygame.init()
-    screen = pygame.display.set_mode((600, 500))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    bg_color = (230, 230, 230)
-
+    
+    # Start the main loop for the game.
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-                
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         pygame.display.flip()
+
 
 run_game()
