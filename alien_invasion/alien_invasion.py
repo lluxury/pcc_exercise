3dd0853f58d7e08b925f964dea15e5d402ee3e0b
yann <tmp4321@qq.com>
2021-03-18 15:38:03 +0800

    add func update_screen

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -18,11 +18,7 @@ def run_game():
     # Start the main loop for the game.
     while True:
         gf.check_events()
-
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -10,3 +10,10 @@ def check_events():
             sys.exit()
 
 
+def update_screen(ai_settings, screen, ship):
+    """Update images on the screen and flip to the new screen."""
+    # Redraw the screen during each pass through the loop.
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
