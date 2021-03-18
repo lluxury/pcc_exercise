62cb54d814ee8941753a514ec1fb774a1bc7c7de
yann <tmp4321@qq.com>
2021-03-18 18:17:30 +0800

    create func create_fleet

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -20,16 +20,17 @@ def run_game():
     ship = Ship(ai_settings, screen)
     # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
 
     alien = Alien(ai_settings, screen)
+    gf.create_fleet(ai_settings, screen, aliens)
 
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
-
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame
 from bullet import Bullet
+from alien import Alien
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Respond to keypresses."""
@@ -47,7 +48,7 @@ def update_bullets(bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
-def update_screen(ai_settings, screen, ship, alien, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
@@ -56,6 +57,22 @@ def update_screen(ai_settings, screen, ship, alien, bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
     # Make the most recently drawn screen visible.
     pygame.display.flip()
+
+
+def create_fleet(ai_settings, screen, aliens):
+    """Create a full fleet of aliens."""
+    # Create an alien, and find number of aliens in a row.
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+
+    # Create the fleet of aliens.
+    for alien_number in range(number_aliens_x):
+        alien= Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
