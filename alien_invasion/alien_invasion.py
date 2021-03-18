dd335d87000e969134eabf93f5860c952d9eb4ee
yann <tmp4321@qq.com>
2021-03-18 18:09:12 +0800

    show alien on the screen

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,9 +1,9 @@
-
-
 import pygame
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
+
 import game_functions as gf
 
 from pygame.sprite import Group
@@ -21,12 +21,15 @@ def run_game():
     # Make a group to store bullets in.
     bullets = Group()
 
+    alien = Alien(ai_settings, screen)
+
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+
 
 
 run_game()
--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -47,7 +47,7 @@ def update_bullets(bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
@@ -56,5 +56,6 @@ def update_screen(ai_settings, screen, ship, bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    alien.blitme()
     # Make the most recently drawn screen visible.
     pygame.display.flip()
