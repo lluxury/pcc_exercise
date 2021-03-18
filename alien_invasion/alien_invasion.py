aa8dd635a18c88c00ae491eac539aad20c1acc94
yann <tmp4321@qq.com>
2021-03-18 14:55:01 +0800

    add bg

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,13 +4,18 @@ import pygame
 def run_game():
     # Initialize pygame, settings, and screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode((600, 500))
     pygame.display.set_caption("Alien Invasion")
+
+    bg_color = (230, 230, 230)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        
+                
+        screen.fill(bg_color)
+        pygame.display.flip()
+
 run_game()
