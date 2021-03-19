8a8e0491e91ad787a7b6db95c21506c3224d55ed
yann <tmp4321@qq.com>
2021-03-19 12:35:13 +0800

    reset game

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -30,7 +30,7 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
         
-def check_events(ai_settings, screen, stats, play_button, ship,
+def check_events(ai_settings, screen, stats,  play_button, ship, aliens,
         bullets):
     """Respond to keypresses and mouse events."""
     for event in pygame.event.get():
@@ -42,13 +42,24 @@ def check_events(ai_settings, screen, stats, play_button, ship,
             check_keyup_events(event, ship)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(stats,  play_button, mouse_x, mouse_y)
+            check_play_button(ai_settings, screen, stats, play_button,
+                          ship, aliens, bullets, mouse_x, mouse_y)
 
-def check_play_button(stats, play_button, mouse_x, mouse_y):
+def check_play_button(ai_settings, screen, stats, play_button, ship,
+        aliens, bullets, mouse_x, mouse_y):
     """Start a new game when the player clicks Play."""
     if play_button.rect.collidepoint(mouse_x, mouse_y):
+        # Reset the game statistics.
+        stats.reset_stats()
         stats.game_active = True
 
+        # Empty the list of aliens and bullets.
+        aliens.empty()
+        bullets.empty()
+
+        # Create a new fleet and center the ship.
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
 
 def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """Update position of bullets, and get rid of old bullets."""
