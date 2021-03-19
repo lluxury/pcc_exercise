c7d5def100f17469537b522450b27f66ac2e94fd
yann <tmp4321@qq.com>
2021-03-19 13:27:50 +0800

    update score

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -68,7 +68,7 @@ def check_play_button(ai_settings, screen, stats, play_button, ship,
         create_fleet(ai_settings, screen, ship, aliens)
         ship.center_ship()
 
-def update_bullets(ai_settings, screen, ship, aliens, bullets):
+def update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """Update position of bullets, and get rid of old bullets."""
     # Update bullet positions.
     bullets.update()
@@ -78,13 +78,20 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
-    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+    check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship,
+        aliens, bullets)
 
 
-def check_bullet_alien_collisions(ai_settings, screen, ship,aliens, bullets):
+def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship,
+        aliens, bullets):
     """Respond to bullet-alien collisions."""
     # Remove any bullets and aliens that have collided.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+    if collisions:
+        for aliens in collisions.values():
+            stats.score +=  ai_settings.alien_points
+            sb.prep_score()
 
     if len(aliens) == 0:
         # Destroy existing bullets, and create new fleet.
