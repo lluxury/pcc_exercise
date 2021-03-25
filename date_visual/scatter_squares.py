cfdd0602c507fe56cc283590dd00574dc4cc11ea
yann <tmp4321@qq.com>
2021-03-25 17:03:15 +0800

    plt a series of points

--- date_visual/scatter_squares.py
+++ date_visual/scatter_squares.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2,4, s=200)
+x_value = [1,2,3,4,5]
+y_values = [1,4,9,16,25]
+plt.scatter(x_value, y_values, s=100)
 
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
