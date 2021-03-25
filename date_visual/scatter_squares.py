3fb3fef42eb6eb89caf1863eb50611a7719dc242
yann <tmp4321@qq.com>
2021-03-25 17:07:48 +0800

    automatic calculation

--- date_visual/scatter_squares.py
+++ date_visual/scatter_squares.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 
-x_value = [1,2,3,4,5]
-y_values = [1,4,9,16,25]
-plt.scatter(x_value, y_values, s=100)
+x_value = list(range(1,1001))
+y_values = [x**2 for x in x_value]
+plt.scatter(x_value, y_values, s=40)
 
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
 
 plt.tick_params(axis="both", which='major', labelsize=14)
+plt.axis([0,1100,0,1100000])
 plt.show()
